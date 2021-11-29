8bd48e1cf08d78717a353968a894e6f993e40696
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2021-11-29 15:43:58 -0300

    new

--- lib/lua.dart
+++ lib/lua.dart
@@ -32,141 +32,139 @@ class _LuaState extends State<Lua> {
     return Material(
       //importante
       color: Colors.white,
-      child: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Expanded(
-              child: SingleChildScrollView(),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              // ignore: prefer_const_literals_to_create_immutables
-              children: [
-                Text(
-                  'Seu peso',
-                  style: TextStyle(
-                    color: Color.fromRGBO(7, 78, 232, 1),
-                    fontWeight: FontWeight.w800,
-                    decoration: TextDecoration.none,
-                    fontSize: 36,
-                  ),
-                )
-              ],
-            ),
-            Container(
-              child: LayoutBuilder(
-                builder: (context, constraints) {
-                  return Stack(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          // ignore: prefer_const_literals_to_create_immutables
-                          children: [
-                            TextField(
-                                keyboardType: TextInputType.number,
-                                controller: _valorController,
-                                decoration: InputDecoration(
-                                  labelText: 'Peso',
-                                  hintText: "Digite o seu peso",
-                                  border: OutlineInputBorder(),
-                                )),
-                            Divider(
-                              height: 20,
-                              thickness: 0,
-                              indent: 20,
-                              endIndent: 20,
-                            ),
-                            Container(
-                              padding: EdgeInsets.symmetric(
-                                  horizontal: 12, vertical: 4),
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(3),
-                                  border: Border.all(
-                                      color: Color.fromRGBO(104, 120, 141, 1),
-                                      width: 1)),
-                              child: DropdownButtonHideUnderline(
-                                //esconde linha do drop
-                                child: DropdownButton<String>(
-                                    //drop
-                                    borderRadius: BorderRadius.circular(10),
-                                    onChanged: (String? newValue) {
-                                      setState(() {
-                                        dropdownValue = newValue!;
-                                      });
-                                    },
-                                    value: dropdownValue,
-                                    isExpanded: true,
-                                    items: listaPlanetas
-                                        .map<DropdownMenuItem<String>>(
-                                            (String value) {
-                                      return DropdownMenuItem(
-                                          value: value,
-                                          child: Text(
-                                            value,
-                                            style: TextStyle(
-                                                fontWeight: FontWeight.w400),
-                                          ));
-                                    }).toList()),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              Text(
+                'Seu peso',
+                style: TextStyle(
+                  color: Color.fromRGBO(7, 78, 232, 1),
+                  fontWeight: FontWeight.w800,
+                  decoration: TextDecoration.none,
+                  fontSize: 36,
+                ),
+              )
+            ],
+          ),
+          LayoutBuilder(
+            builder: (context, constraints) {
+              return Stack(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      // ignore: prefer_const_literals_to_create_immutables
+                      children: [
+                        TextField(
+                            keyboardType: TextInputType.number,
+                            controller: _valorController,
+                            decoration: InputDecoration(
+                              labelText: 'Peso',
+                              hintText: "Digite o seu peso",
+                              border: OutlineInputBorder(),
+                            )),
+                        Divider(
+                          height: 20,
+                          thickness: 0,
+                          indent: 20,
+                          endIndent: 20,
+                        ),
+                        Container(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 12, vertical: 4),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(3),
+                              border: Border.all(
+                                  color: Color.fromRGBO(104, 120, 141, 1),
+                                  width: 1)),
+                          child: DropdownButtonHideUnderline(
+                            //esconde linha do drop
+                            child: DropdownButton<String>(
+                                //drop
+                                borderRadius: BorderRadius.circular(10),
+                                onChanged: (String? newValue) {
+                                  setState(() {
+                                    dropdownValue = newValue!;
+                                  });
+                                },
+                                value: dropdownValue,
+                                isExpanded: true,
+                                items: listaPlanetas
+                                    .map<DropdownMenuItem<String>>(
+                                        (String value) {
+                                  return DropdownMenuItem(
+                                      value: value,
+                                      child: Text(
+                                        value,
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.w400),
+                                      ));
+                                }).toList()),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: SizedBox(
+                            height: 50,
+                            width: MediaQuery.of(context).size.width * .25,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius:
+                                        BorderRadius.circular(35.0),
+                                    side: BorderSide(color: Colors.white)),
+                                primary: Color.fromRGBO(7, 78, 232, 1),
+                                elevation: 0,
+                                onPrimary: Colors.white, // foreground
                               ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: SizedBox(
-                                height: 50,
-                                width: MediaQuery.of(context).size.width * .25,
-                                child: ElevatedButton(
-                                  style: ElevatedButton.styleFrom(
-                                    shape: RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(35.0),
-                                        side: BorderSide(color: Colors.white)),
-                                    primary: Color.fromRGBO(7, 78, 232, 1),
-                                    elevation: 0,
-                                    onPrimary: Colors.white, // foreground
-                                  ),
-                                  onPressed: () {
-                                    setState(() {
-                                      //mudar o valor do contator instantaneamente
-                                      contator++;
-                                    });
-                                    if (_valorController.text.isNotEmpty) {
-                                      // ignore: unused_local_variable
-                                      final double valor =
-                                          double.parse(_valorController.text);
-                                      Total(valor, dropdownValue);
-                                    }
-                                  },
-                                  child: Text(
-                                    "Calcular",
-                                    style: TextStyle(
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w500,
-                                    ),
-                                  ),
+                              onPressed: () {
+                                setState(() {
+                                  //mudar o valor do contator instantaneamente
+                                  contator++;
+                                });
+                                if (_valorController.text.isNotEmpty) {
+                                  // ignore: unused_local_variable
+                                  final double valor =
+                                      double.parse(_valorController.text);
+                                  Total(valor, dropdownValue);
+                                }
+                              },
+                              child: Text(
+                                "Calcular",
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w500,
                                 ),
                               ),
                             ),
-                            ImagemPlanetas(dropdownValue),
-                            TextoPlaneta()
-                          ],
+                          ),
                         ),
-                      ),
-                    ],
-                  );
-                },
-              ),
-            )
-          ],
-        ),
+                        ImagemPlanetas(dropdownValue),
+                        TextoPlaneta()
+                      ],
+                    ),
+                  ),
+                ],
+              );
+            },
+          )
+        ],
       ),
     );
   }
 
   // ignore: non_constant_identifier_names
   SizedBox TextoPlaneta() {//aquie
+ // final double valor = double.parse(_valorController.text);
+
     if (_valorController.text.isNotEmpty && contator > 0) {
       return SizedBox(
         height: MediaQuery.of(context).size.height * .10,
